8410bc98900edf2f85d6e1c6e2448fda527186da
yaojinhui <yaojinhui1993@gmail.com>
2019-01-04 11:33:40 +0800

    autocomplete

---
+++ src/pages/AutoComplete.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { AutoComplete } from 'antd';
+
+function onSelect(value) {
+  console.log('onSelect', value);
+}
+
+class AutoCompletePage extends Component {
+  state = {
+    dataSource: [],
+  }
+
+  handleSearch = (value) => {
+    this.setState({
+      dataSource: !value ? [] : [
+        value,
+        value + value,
+        value + value + value
+      ]
+    });
+  }
+
+  render() {
+    const { dataSource } = this.state;
+
+    return (
+      <AutoComplete
+        dataSource={dataSource}
+        style={{ width: 200 }}
+        onSelect={onSelect}
+        onSearch={this.handleSearch}
+        placeholder="input here"
+      />
+    )
+  }
+
+}
+
+export default AutoCompletePage;
