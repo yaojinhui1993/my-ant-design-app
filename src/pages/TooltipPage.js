0cbf37ebd3aa8550e1b557a23d5d5e3d6ae4b31b
yaojinhui <yaojinhui1993@gmail.com>
2019-01-06 14:32:27 +0800

    Tooltip

---
+++ src/pages/TooltipPage.js
@@ -0,0 +1,9 @@
+import { Tooltip } from 'antd';
+
+export default function TooltipPage() {
+  return (
+    <Tooltip title="prompt" text>
+      <span>Lorem ipsum dolor sit amet.</span>
+    </Tooltip>
+  );
+}
