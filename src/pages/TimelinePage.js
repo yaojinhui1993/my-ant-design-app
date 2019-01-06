920b25029f54b4cc9353761f767974b3fa1b5a60
yaojinhui <yaojinhui1993@gmail.com>
2019-01-06 14:52:59 +0800

    Timeline

---
+++ src/pages/TimelinePage.js
@@ -0,0 +1,12 @@
+import { Timeline } from 'antd';
+
+export default function TimelinePage() {
+  return (
+    <Timeline>
+      <Timeline.Item>Create a services site</Timeline.Item>
+      <Timeline.Item>Solve initial network problems</Timeline.Item>
+      <Timeline.Item>Technical testing</Timeline.Item>
+      <Timeline.Item>Network problems</Timeline.Item>
+    </Timeline>
+  );
+}
