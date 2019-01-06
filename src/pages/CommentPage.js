85d128ea4d6b51bb1706a5aaf790aacca6e72fc1
yaojinhui <yaojinhui1993@gmail.com>
2019-01-06 11:35:39 +0800

    Comment

---
+++ src/pages/CommentPage.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import { Comment, Icon, Tooltip, Avatar } from 'antd';
+import moment from 'moment';
+
+class CommentPage extends Component {
+  state = {
+    likes: 0,
+    dislikes: 0,
+    action: null,
+  };
+
+  like = () => {
+    this.setState({
+      likes: 1,
+      dislikes: 0,
+      action: 'liked',
+    });
+  };
+
+  dislikes = () => {
+    this.setState({
+      likes: 0,
+      dislikes: 1,
+      action: 'disliked',
+    });
+  };
+
+  render() {
+    const { likes, dislikes, action } = this.state;
+
+    const actions = [
+      <span>
+        <Tooltip title="Like">
+          <Icon
+            type="like"
+            theme={action === 'liked' ? 'filled' : 'outlined'}
+            onClick={this.like}
+          />
+        </Tooltip>
+        <span style={{ paddingLeft: 8, cursor: 'auto' }}>{likes}</span>
+      </span>,
+      <span>
+        <Tooltip title="Dislike">
+          <Icon
+            type="dislike"
+            theme={action === 'disliked' ? 'filled' : 'outlined'}
+            onClick={this.dislike}
+          />
+        </Tooltip>
+        <span style={{ paddingLeft: 8, cursor: 'auto' }} />
+      </span>,
+      <span>Reply to</span>,
+    ];
+    return (
+      <Comment
+        actions={actions}
+        author={<a>Han Solo</a>}
+        avatar={
+          <Avatar
+            src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+            alt="Han Solo"
+          />
+        }
+        content={
+          <p>
+            We supply a series of design principles, practical patterns and high
+            quality design resources...
+          </p>
+        }
+        datetime={
+          <Tooltip title={moment().format('YYYY-MM-DD HH:mm:ss')}>
+            <span>{moment().fromNow()}</span>
+          </Tooltip>
+        }
+      />
+    );
+  }
+}
+
+export default CommentPage;
