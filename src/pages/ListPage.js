185630c6b7bdb24e86a9fd8e4784589fc01c6b2a
yaojinhui <yaojinhui1993@gmail.com>
2019-01-06 13:55:39 +0800

    List

---
+++ src/pages/ListPage.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { List } from 'antd';
+
+const data = [
+  'Racing car sprays burning fuel into crowd.',
+  'Japanese princess to wed commoner.',
+  'Australian walks 100km after outback crash.',
+  'Man charged over missing wedding girl.',
+  'Los Angeles battles huge wildfires.',
+];
+
+class ListPage extends Component {
+  render() {
+    return (
+      <div>
+        <h3 style={{ marginBottom: 16 }}> Default Size</h3>
+        <List
+          header={<div>Hearder</div>}
+          footer={<div>Footer</div>}
+          bordered
+          dataSource={data}
+          renderItem={item => <List.Item>{item}</List.Item>}
+        />
+
+        <h3 size={{ margin: '16px 0' }}>Small Size</h3>
+        <List
+          size="small"
+          header={<div>Header</div>}
+          footer={<div>Footer</div>}
+          bordered
+          dataSource={data}
+          renderItem={item => <List.Item>{item}</List.Item>}
+        />
+
+        <h3 style={{ margin: '16px 0' }}>Large Size</h3>
+        <List
+          size="large"
+          header={<div>Header</div>}
+          footer={<div>Footer</div>}
+          bordered
+          dataSource={data}
+          renderItem={item => <List.Item>{item}</List.Item>}
+        />
+      </div>
+    );
+  }
+}
+
+export default ListPage;
