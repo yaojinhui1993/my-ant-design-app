dd6d93765cdadb1fa658c1ba1fdbde7eb0a82f0b
yaojinhui <yaojinhui1993@gmail.com>
2019-01-08 19:04:52 +0800

    Popconfirm

---
+++ src/pages/PopconfirmPage.js
@@ -0,0 +1,25 @@
+import { Popconfirm, message } from 'antd';
+
+function confirm(e) {
+  console.log(e);
+  message.success('Click on Yes');
+}
+
+function cancel(e) {
+  console.log(e);
+  message.error('Click on No');
+}
+
+export default function PopconfirmPage() {
+  return (
+    <Popconfirm
+      title="Are you sure delete this task?"
+      onConfirm={confirm}
+      onCancel={cancel}
+      okText="Yes"
+      cancelText="No"
+    >
+      <a href="#">Delete</a>
+    </Popconfirm>
+  );
+}
